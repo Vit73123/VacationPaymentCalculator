1aae02e558fef87afa32969167ee6d7738da2829
vit73123 <vit73123@gmail.com>
2025-04-03 17:06:00 +0300

    init

---
+++ src/main/java/com/example/VacationPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationPaymentApplication.class, args);
+    }
+
+}
